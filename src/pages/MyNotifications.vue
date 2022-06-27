9de6f96af037bf98ffce64d5920f3d7ed09f13bf
Priya X. Pramesi <ppramesi@visi8.com>
2022-06-27 18:15:29 +0700

    notif

--- src/pages/MyNotifications.vue
+++ src/pages/MyNotifications.vue
@@ -5,16 +5,19 @@
                 <div class="flex flex-row">
                     <div class="flex flex-col w-full">
                         <div class="flex flex-row">
-                            <div>
+                            <div :class="notif.unread ? '' : 'image-overlay'">
                                 <img class="h-28 max-w-auto" style="max-width: fit-content;" :src="notif.chapter_preview_url" alt="">
                             </div>
                             <div class="px-3 flex flex-col flex-grow items-start justify-center gap-y-1">
-                                <span class="font-extrabold text-sm leading-3 lg:text-md xl:text-lg">Comic Update: {{ notif.title }} Chapter {{notif.chapter_number}}</span> <span class="=lg:text-sm xl:text-md text-xs text-gray-900 leading-3">Release date: {{ notif.last_update.toDate().toLocaleDateString('id-ID') }}</span>
+                                <span class="font-extrabold text-sm leading-3 lg:text-md xl:text-lg">Comic Update: {{ notif.title }} Chapter {{notif.chapter_number}}</span> <span class="=lg:text-sm xl:text-md text-xs text-gray-900 leading-3">Release date: {{ notif.created_date.toDate().toLocaleDateString('id-ID') }}</span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div v-if="enableLoadMore" class="flex items-center justify-center">
+            <div class="bg-green-500 hover:bg-green-600 focus:outline-none active:ring-1 active:ring-green-300 active:bg-green-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white mt-5" @click="loadMore">load more</div>
         </div>
     </div>
 </template>
@@ -23,7 +26,6 @@
 import { useAuthStore } from '@/store/auth'
 import { mapState } from 'pinia'
 import Comic from '@/firebase/comics/Comic.js'
-import Chapter from '@/firebase/comics/Chapter.js'
 import _ from 'lodash'
 export default{
     name: 'my-notifications',
@@ -34,12 +36,12 @@ export default{
         });
 
         if(this.userInstance){
-            this.getNotifications().then(_.over([this.clearUnread, this.processNotifs]))
+            this.getNotifications().then(_.over([this.processNotifs, this.clearUnread]))
         }
     },
     watch: {
         userInstance(){
-            this.getNotifications().then(_.over([this.clearUnread, this.processNotifs]))
+            this.getNotifications().then(_.over([this.processNotifs, this.clearUnread]))
         }
     },
     data(){
@@ -48,6 +50,7 @@ export default{
             processedNotifs: [],
             loader: null,
             once: true,
+            enableLoadMore: false
         }
     },
     computed: {
@@ -57,8 +60,10 @@ export default{
         goToComic(comicRef){
             this.$router.push(this.routeResolver('Comic', {id: comicRef.id}))
         },
-        processNotifs(){
-            const test = this.notifs.map(async (notif) => {
+        processNotifs(notifs){
+            let notifCount = 0
+            const test = notifs.map(async (notif) => {
+                notifCount += 1
                 const comicPromise = Comic.getDocumentWithStorageResourceUrl(notif.comic.id, ['cover_image_url'])
                 //eslint-disable-next-line no-unused-vars
                 const [ root, comicId, pathOne, chapterId ] = notif.chapter.path.split('/')
@@ -67,8 +72,17 @@ export default{
                     const currentChapter = cmc.chapters_data.find(v => v.id == chapterId) ?? {}
                     this.processedNotifs.push({
                         ...cmc,
+                        ...currentChapter,
                         ...notif,
-                        ...currentChapter
+                    })
+                    this.processedNotifs.sort((a, b) => {
+                        if(a.created_date > b.created_date){
+                            return -1
+                        }
+                        if(a.created_date < b.created_date){
+                            return 1
+                        }
+                        return 0
                     })
                     if(this.once){
                         this.loader.hide()
@@ -77,11 +91,21 @@ export default{
                 })
             })
             Promise.all(test).then(() => {
-                console.log(this.processedNotifs)
+                if(notifCount < 10){
+                    this.enableLoadMore = false
+                }else{
+                    this.enableLoadMore = true
+                }
             })
+        },
+        async loadMore(){
+            const moreNotifs = await this.userInstance.getNotifications('comic', 10, this.notifs[this.notifs.length - 1].doc)
+            this.notifs = [...this.notifs, ...moreNotifs]
+            this.processNotif(moreNotifs)
         },
         async getNotifications(){
             this.notifs = await this.userInstance.getNotifications()
+            return this.notifs
         },
         async clearUnread(){
             const unreadNotifs = this.notifs.filter(v => v.unread)
@@ -93,6 +117,16 @@ export default{
 }
 </script>
 
-<style>
+<style scoped>
+.image-overlay::before{
+    content:"";
+    position: absolute;
+    top:0;
+    right:0;
+    left:0;
+    bottom:0;
+    z-index:1;
+    background:rgba(0, 0, 0, 0.4);
+}
 
 </style>
